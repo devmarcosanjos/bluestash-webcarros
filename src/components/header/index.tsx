8cbea03628c1f4d2668eed6e8d84ca516358cac5
marcos <dev.marcosanjos@gmail.com>
2025-05-01 17:28:51 -0300

    Implement code changes to enhance functionality and improve performance

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,33 @@
+import { Link } from 'react-router';
+import logo from '../../assets/BlueStashWebCarros.svg';
+import { LogIn, User } from 'lucide-react';
+
 export function Header() {
+  const singed = false;
+  const loadingAuth = false;
+
   return (
-    <div className="">
-      <h1>COMPONENTE HEADER</h1>
+    <div className="w-full flex item justify-center h-16 bg-white drop-shadow mb-4">
+      <header className="flex w-full justify-between max-w-7xl items-center px-4">
+        <Link
+          to="/"
+          className="flex items-center w-[256px] bg-red-600 h-12 px-4 rounded-sm"
+        >
+          <img src={logo} alt="Bluestash Web Carros" />
+        </Link>
+
+        {!loadingAuth && singed && (
+          <Link to="/dashboard">
+            <User size={24} />
+          </Link>
+        )}
+
+        {!loadingAuth && !singed && (
+          <Link to="/login">
+            <LogIn size={24} />
+          </Link>
+        )}
+      </header>
     </div>
   );
 }
