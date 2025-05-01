72e42f4cb73a973ad942e6f2eeccc61fd9705dab
marcos <dev.marcosanjos@gmail.com>
2025-05-01 15:39:32 -0300

    chore: update tailwind

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/BlueStash WebCarros.svg';
+import { UserCircle } from 'lucide-react';
+
 export function Header() {
+  const singned = false;
+  const loadingAuth = false;
+
+
   return (
-    <div>
-      <h1>Header</h1>
+    <div className='w-full flex items-center justify-between '>
+      <header>
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+
+        <Link to="/login">
+          <UserCircle />
+        </Link>
+      </header>      
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@pages': resolve(__dirname, './src/pages'), // corrected 'page' to 'pages'
+      '@assets': resolve(__dirname, './src/assets')
+    }
+  },
+  css: {
+    postcss: './postcss.config.js'
+  }
 })
